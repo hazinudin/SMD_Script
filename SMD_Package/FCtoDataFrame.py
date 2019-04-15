c5a40efdeab0c54b5962b2821ae9475b45490c46
hazinudin <hazinudin@hotmail.com>
2019-04-15 11:28:50 +0700

    Minor Fix

--- SMD_Package/FCtoDataFrame.py
+++ SMD_Package/FCtoDataFrame.py
@@ -1,4 +1,4 @@
-from arcpy import da, env
+from arcpy import da, env, ListFields, AddMessage
 from pandas import DataFrame
 
 
@@ -33,8 +33,10 @@ def event_fc_to_df(gdb_table, search_field, route_selection, route_identifier, s
     # Modify the where_clause to prevent null event row with null segment to be included
     if is_table:  # If the inputted is an SDE Table without geometry then include all records
         pass
-    elif not include_all:
+    elif not include_all and (route_selection != 'ALL'):
         where_clause += "AND (SHAPE.LEN IS NOT NULL)"
+    elif not include_all and (route_selection == 'ALL'):
+        where_clause = 'SHAPE.LEN IS NOT NULL'
 
     # Create the sql_clause for DataAccess module
     if orderby is None:
@@ -46,6 +48,16 @@ def event_fc_to_df(gdb_table, search_field, route_selection, route_identifier, s
             orderby = str(orderby).strip('[]')
 
         sql_clause = (None, 'ORDER BY {0}'.format(orderby))
+
+    # Find shape field in the search_field, if exist then pop it
+    if search_field == '*':
+        lf = ListFields(gdb_table)
+        search_field = [x.name for x in lf]
+
+    if type(search_field) == list:
+        for field in search_field:
+            if 'shape' in field.lower():
+                search_field.remove(field)
 
     # Create the numpy array of the requested table or feature class from GeoDataBase
     table_search = da.FeatureClassToNumPyArray(gdb_table, search_field, where_clause=where_clause,
