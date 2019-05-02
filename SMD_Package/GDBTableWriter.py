bd13ab98d92055854f2d14608ed01515ef4ac6b2
hazinudin <hazinudin@hotmail.com>
2019-05-02 10:31:02 +0700

    Add New Feature to GDBTableWriter.py
    - Delete row if the route already exists.
    - Create new table if the table does not exists.

--- SMD_Package/GDBTableWriter.py
+++ SMD_Package/GDBTableWriter.py
@@ -1,9 +1,10 @@
-from arcpy import da, env, CreateTable_management, AddField_management
+from arcpy import da, env, CreateTable_management, AddField_management, Exists
 
 env.overwriteOutput = True
 
 
-def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=False):
+def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=False, input_routeid='LINKID',
+                     target_routeid='LINKID'):
     """
     This function writes input DataFrame as geodatabase event table
     :param workspace: The workspace for target database table
@@ -12,17 +13,32 @@ def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=Fal
     :param table_name: The target table name
     :param cols_dtype: The colums of target table name, if new 'new_table' is True then the column will be used to
     create new column in the newly created table.
+    :param input_routeid: The RouteID column in the input table.
+    :param target_routeid: The RouteID column in the target table.
     :return:
     """
+    env.workspace = workspace  # Environment workspace
+    env.overwriteOutput = True
+    table_exist = Exists(table_name)  # Check if the table already exist in the workspace.
 
-    if new_table:
+    if new_table or (not table_exist):  # If the table does not exist or new_table is True, then create new table.
         CreateTable_management(workspace, table_name)
         for col in cols_dtype.keys():
-            AddField_management(table_name, col, cols_dtype[col])
+            AddField_management(table_name, col, cols_dtype[col]['dtype'])
 
-    cursor = da.InsertCursor(table_name, cols_dtype.keys())
-    for index, row in dataframe.iterrows():
-        row_object = []
-        for col_name in cols_dtype.keys():
-            row_object.append(row[col_name])
-        cursor.insertRow(row_object)
+    input_routes = dataframe[input_routeid].unique().tolist()  # List of every route in the input DataFrame
+
+    for route in input_routes:  # Iterate for every available route in the input
+        df_route = dataframe.loc[dataframe[input_routeid] == route]  # The route DataFrame
+
+        with da.UpdateCursor(table_name, target_routeid, where_clause="{0}='{1}'".format(target_routeid, route))\
+                as del_cursor:
+            for _ in del_cursor:
+                del_cursor.deleteRow()  # If the route already exist in the table then delete the whole route row
+
+        cursor = da.InsertCursor(table_name, cols_dtype.keys())  # Create an insert cursor
+        for index, row in df_route.iterrows():  # Iterate over available rows
+            row_object = []  # Create the new row object
+            for col_name in cols_dtype.keys():
+                row_object.append(row[col_name])
+            cursor.insertRow(row_object)  # Insert the new row
