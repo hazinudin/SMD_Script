46d042d8687de5b8bab522f586c299ea018d9d3d
hazinudin <hazinudin@hotmail.com>
2019-05-14 16:09:26 +0700

    Create CSV File Output For All Error Message

--- Testing/RNICurrentDataCheck.py
+++ Testing/RNICurrentDataCheck.py
@@ -4,7 +4,7 @@ sys.path.append('E:/SMD_Script')
 os.chdir('E:/SMD_Script')
 from SMD_Package import EventValidation
 from pandas import DataFrame
-from arcpy import da, env
+from arcpy import da, env, GetParameterAsText, SetParameter
 import json
 
 
@@ -13,9 +13,10 @@ with open('RNICheck/rni_config.json') as config_f:
     rni_config = json.load(config_f)
 
 env.workspace = 'Database Connections/ELRS@GEODBBM 144.sde'
+env.overwriteOutput = True
 rni_fc = 'ELRS.RNI_National_2'
 input_fields = ['LINKID', 'KMPOST', 'KMPOSTTO', 'LANE_CODE', 'ROAD_TYPE', 'MEDWIDTH']
-prov_code = '62'
+prov_code = GetParameterAsText(0)
 
 # Create the array
 ar = da.FeatureClassToNumPyArray(rni_fc, input_fields, where_clause="NOPROP = '{0}'".format(prov_code))
@@ -35,4 +36,5 @@ TableCheck = EventValidation(df, column_details, 'ELRS.National_Network2018', 'R
 TableCheck.measurement_check(rni_fc, 'LINKID', 'TOMEASURE', from_m_col='KMPOST', to_m_col='KMPOSTTO', lane_code='LANE_CODE')
 TableCheck.rni_roadtype_check(roadtype_details, from_m_col='KMPOST', to_m_col='KMPOSTTO', lane_codes='LANE_CODE')
 
-print TableCheck.altered_route_result()
+DataFrame(TableCheck.altered_route_result()).to_csv("{1}/error_check_{0}.csv".format(prov_code, env.scratchFolder))
+SetParameter(1, "{1}/error_check_{0}.csv".format(prov_code, env.scratchFolder))
