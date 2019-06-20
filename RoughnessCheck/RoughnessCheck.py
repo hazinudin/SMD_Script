d40a0afa2489d93ca88ada6868f6cd8ff7e595ce
hazinudin <hazinudin@hotmail.com>
2019-06-20 11:20:46 +0700

    Move back the Output Table Config from SMD Config File to Roughness Config File.

--- RoughnessCheck/RoughnessCheck.py
+++ RoughnessCheck/RoughnessCheck.py
@@ -48,7 +48,7 @@ IRIColumn = "IRI"
 RouteIDCol = 'LINKID'
 
 # The GDB table which store all the valid table row
-OutputGDBTable = smd_config['output_table']['roughness']
+OutputGDBTable = roughness_config['output_table']
 
 # All the details about comparison table
 CompTable = roughness_config['compare_table']['table_name']
