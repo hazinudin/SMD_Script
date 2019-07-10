897b7bea1449e9c47c6ab2fe714dc41df44503ba
hazinudin <hazinudin@hotmail.com>
2019-07-10 15:17:23 +0700

    Add Input Lane Code and RNI Lane Code Parameter to Kemantapan Class.

--- SMD_Package/EventTable/Kemantapan.py
+++ SMD_Package/EventTable/Kemantapan.py
@@ -5,8 +5,9 @@ from SMD_Package.FCtoDataFrame import event_fc_to_df
 
 
 class Kemantapan(object):
-    def __init__(self, df_rni, df_event, grading_col, route_col, from_m_col, to_m_col, rni_route_col, rni_from_col,
-                 rni_to_col, surftype_col=None, kemantapan_type='ROUGHNESS', lane_based=False):
+    def __init__(self, df_rni, df_event, grading_col, route_col, from_m_col, to_m_col, lane_code, rni_route_col,
+                 rni_from_col, rni_to_col, rni_lane_code, surftype_col=None, kemantapan_type='ROUGHNESS',
+                 lane_based=False):
         """
         Initialize the Kemantapan class for grading kemantapan value
         :param df_rni: The DataFrame for RNI table.
@@ -47,7 +48,7 @@ class Kemantapan(object):
 
         # The input and RNI DataFrame merge result
         merge_df = self.rni_table_join(df_rni, df_event, route_col, from_m_col, to_m_col, grading_col,
-                                       rni_route_col, rni_from_col, rni_to_col, surftype_col)
+                                       rni_route_col, rni_from_col, rni_to_col, surftype_col, lane_based)
         self.graded_df = self.grading(merge_df, surftype_col, grading_col, group_details, kemantapan_type)
         self.mantap_percent = self.kemantapan_percentage(self.graded_df, route_col, from_m_col, to_m_col)
 
@@ -297,7 +298,7 @@ class Kemantapan(object):
 
     @staticmethod
     def rni_table_join(df_rni, df_event, route_col, from_m_col, to_m_col, grading_col, rni_route_col, rni_from_col,
-                       rni_to_col, surftype_col, match_only=True):
+                       rni_to_col, surftype_col, lane_based, match_only=True):
         """
         This static method used for joining the input event table and the RNI table
         :param df_rni: The RNI DataFrame.
@@ -313,24 +314,29 @@ class Kemantapan(object):
         :param match_only: If True then this method only returns the 'both' merge result.
         :return: A DataFrame from the merge result between the RNI and input event table.
         """
-        input_group_col = [route_col, from_m_col, to_m_col]  # The column used for input groupby
-        rni_group_col = [rni_route_col, rni_from_col, rni_to_col]  # The column used for the RNI groupby
-        df_rni[surftype_col] = pd.Series(df_rni[surftype_col].astype(int))  # Convert the surftype to integer type
+        if not lane_based:  # Do the table join with linkid, from, and to as join key.
+            input_group_col = [route_col, from_m_col, to_m_col]  # The column used for input groupby
+            rni_group_col = [rni_route_col, rni_from_col, rni_to_col]  # The column used for the RNI groupby
+            df_rni[surftype_col] = pd.Series(df_rni[surftype_col].astype(int))  # Convert the surftype to integer type
 
-        # GroupBy the input event DataFrame to make summarize the value used for grading from all lane.
-        input_groupped = df_event.groupby(by=input_group_col)[grading_col].mean().reset_index()
+            # GroupBy the input event DataFrame to make summarize the value used for grading from all lane.
+            input_groupped = df_event.groupby(by=input_group_col)[grading_col].mean().reset_index()
 
-        # GroupBy the RNI Table to get the summary of surface type from all lane in a segment.
-        # Get the first surface type in single RNI segment group
-        rni_groupped = df_rni.groupby(by=rni_group_col)[surftype_col].\
-            agg(lambda x: x.value_counts().index[0]).reset_index()
+            # GroupBy the RNI Table to get the summary of surface type from all lane in a segment.
+            # Get the first surface type in single RNI segment group
+            rni_groupped = df_rni.groupby(by=rni_group_col)[surftype_col].\
+                agg(lambda x: x.value_counts().index[0]).reset_index()
 
-        # Merge the RNI DataFrame and the event DataFrame
-        df_merge = pd.merge(input_groupped, rni_groupped, how='outer', left_on=input_group_col, right_on=rni_group_col,
-                            indicator=True, suffixes=['_INPUT', '_RNI'])
+            # Merge the RNI DataFrame and the event DataFrame
+            df_merge = pd.merge(input_groupped, rni_groupped, how='outer', left_on=input_group_col, right_on=rni_group_col,
+                                indicator=True, suffixes=['_INPUT', '_RNI'])
 
-        df_match = df_merge.loc[df_merge['_merge'] == 'both']  # DataFrame for only match segment interval
-        return df_match if match_only else df_merge  # If 'match_only' is true then only return the 'both'
+            df_match = df_merge.loc[df_merge['_merge'] == 'both']  # DataFrame for only match segment interval
+            return df_match if match_only else df_merge  # If 'match_only' is true then only return the 'both'
+
+        elif lane_based:  # Do the table join with linkid, from, to and lane code as join key
+            input_key = [route_col, from_m_col, to_m_col]
+            pass
 
     @staticmethod
     def grading(df_merge, surftype_col, grading_col, group_details, kemantapan_type, grading_result='_grade'):
