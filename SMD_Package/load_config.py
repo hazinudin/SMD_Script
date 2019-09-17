1325f85c2adc24a157b0442fa0e710ac0dd59d48
hazinudin <hazinudin@hotmail.com>
2019-09-17 13:59:52 +0700

    Load_config initial commit.

---
+++ SMD_Package/load_config.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+
+class Configs(object):
+    def __init__(self, config_file='smd_config.json'):
+        """
+        This class will load all the first level keys as class attributes.
+        """
+        file_path = os.path.join(os.getcwd(), '..', config_file)
+
+        with open(file_path) as config_f:  # Open the config file in the root directory
+            config_dict = json.load(config_f)
+
+        for keys, value in config_dict.items():  # Assign dictionary keys to a class attribute
+            setattr(self, keys, value)
