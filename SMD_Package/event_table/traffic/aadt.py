708d5f2646e93f71b4d4cf453664901e99caeb5b
hazinudin <hazinudin@hotmail.com>
2019-10-25 11:14:05 +0700

    Load the multiplier JSON with appended module directory.

--- SMD_Package/event_table/traffic/aadt.py
+++ SMD_Package/event_table/traffic/aadt.py
@@ -4,14 +4,16 @@ This script contains class and function used to calculate AADT from RTC data.
 import json
 import pandas as pd
 import numpy as np
+import os
 
 
 def _traffic_multiplier(dataframe, survey_date, col_prefix='VEH'):
     df = dataframe
     columns = df.columns.tolist()
     veh_columns = np.char.startswith(columns, col_prefix)
+    directory = os.path.dirname(__file__)
 
-    with open("traffic_multiplier.json") as f:  # Load the multiplier JSON file
+    with open(directory + "/traffic_multiplier.json") as f:  # Load the multiplier JSON file
         _multiplier = json.load(f)
         multiplier = pd.DataFrame.from_dict(_multiplier, orient='index')  # Load as pandas DataFrame
         multiplier_col = '_multiplier'
