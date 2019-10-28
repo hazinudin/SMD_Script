17cfcb2851cb2dd2decfa8ab2ebd00e17087a76a
hazinudin <hazinudin@hotmail.com>
2019-10-28 15:48:03 +0700

    Add daily_aadt class method which return daily AADT summary either for route or direction based.

--- SMD_Package/event_table/traffic/aadt.py
+++ SMD_Package/event_table/traffic/aadt.py
@@ -5,6 +5,7 @@ import json
 import pandas as pd
 import numpy as np
 import os
+import datetime
 
 
 class AADT(object):
@@ -20,6 +21,20 @@ class AADT(object):
 
         self.df_multiplied = self._traffic_multiplier()
 
+    def daily_aadt(self, lane_aadt=True):
+        veh_columns = self.veh_columns
+        df = self._add_survey_time()
+        df.set_index('_survey_time', inplace=True)  # Set the survey time as index
+        grouped = df.groupby(by=[self.routeid_col, self.survey_direction])
+        resample_result = grouped[veh_columns].apply(lambda x: x.resample('1440T', label='right',
+                                                     base=(x.index.min().hour*60)+x.index.min().minute).
+                                                     sum().reset_index(drop=True).mean())
+
+        if lane_aadt:
+            return resample_result.reset_index()  # Lane AADT
+        else:
+            return resample_result.reset_index().groupby(by='LINKID').sum().reset_index()  # Route AADT
+
     def _traffic_multiplier(self):
         df = self.df.copy(deep=True)
         survey_date = self.date_col
@@ -32,11 +47,21 @@ class AADT(object):
             multiplier_col = '_multiplier'
             multiplier.columns = [multiplier_col]  # Rename the column
 
-        df['_day'] = df[survey_date].dt.dayofweek.astype(
-            str)  # Create column for storing day (0 is Monday - 6 Sunday)
+        df['_day'] = df[survey_date].dt.dayofweek.astype(str)  # Create column for storing day (0 is Monday - 6 Sunday)
         df = df.join(multiplier, on='_day')
         df.loc[:, veh_columns] = df.apply(lambda x: x[veh_columns] * x[multiplier_col],
                                           axis=1)  # Multiply every VEH column
+
+        return df
+
+    def _add_survey_time(self, column='_survey_time'):
+        df = self.df_multiplied.copy(deep=True)
+        survey_date = self.date_col
+        hour = self.hour_col
+        minute = self.minute_col
+
+        # Add new column
+        df[column] = df.apply(lambda x: x[survey_date] + datetime.timedelta(hours=x[hour], minutes=x[minute]), axis=1)
 
         return df
 
