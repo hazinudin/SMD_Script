f9b2365be89bc60dc41bfbfc661ff661abb84eaa
hazinudin <hazinudin@hotmail.com>
2019-12-03 14:35:01 +0700

    Update to latest function and class parameter.

--- WriteKemantapan/KemantapanToGDB.py
+++ WriteKemantapan/KemantapanToGDB.py
@@ -14,28 +14,21 @@ with open('smd_config.json') as config_f:
 routeSelection = request_j['routes']
 
 dbConnection = smd_config['smd_database']['instance']
-rniTable = smd_config['table_names']['rni']
-rniRouteID = smd_config['table_fields']['rni']['route_id']
-rniFromMeasure = smd_config['table_fields']['rni']['from_measure']
-rniToMeasure = smd_config['table_fields']['rni']['to_measure']
-rniSurfType = smd_config['table_fields']['rni']['surface_type']
 
-iriTable = 'ELRS.Roughness_National_2'
+iriTable = 'ELRS.RAW_IRI_2019_1_20191106'
 iriRouteID = 'LINKID'
 iriFromMeasure = 'KMPOST'
 iriToMeasure = 'KMPOSTTO'
 iriGrade = 'IRI'
+iriLaneCode = 'LANE_CODE'
 
 columnDetails = dict()
 
 env.workspace = dbConnection
-rniDf = event_fc_to_df(rniTable, [rniRouteID, rniFromMeasure, rniToMeasure, rniSurfType], routeSelection, rniRouteID,
-                       dbConnection, is_table=True)
 iriDf = event_fc_to_df(iriTable, [iriRouteID, iriFromMeasure, iriToMeasure, iriGrade], routeSelection, iriRouteID,
-                       dbConnection, is_table=False, include_all=False)
+                       dbConnection, is_table=True)
 
-kemantapan = Kemantapan(rniDf, iriDf, iriGrade, iriRouteID, iriFromMeasure, iriToMeasure, rniRouteID, rniFromMeasure,
-                        rniToMeasure, surftype_col=rniSurfType)
+kemantapan = Kemantapan(iriDf, iriGrade, iriRouteID, iriFromMeasure, iriToMeasure, iriLaneCode, 'ROUGHNESS')
 summaryTable = kemantapan.summary().reset_index()
 
 # Create the column details
@@ -53,4 +46,4 @@ for col_name in summaryTable.dtypes.to_dict():
     columnDetails[col_name]['dtype'] = gdb_dtype  # Insert the column data type
 
 # Write to GDB.
-gdb_table_writer(dbConnection, summaryTable, 'kemantapan_2018_DEV', columnDetails, new_table=False)
+gdb_table_writer(dbConnection, summaryTable, 'kemantapan_2019_1_DEV', columnDetails, new_table=False)
