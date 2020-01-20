8e81f7ad76302623c5e6037c5126ef051ea9cbc9
hazinudin <hazinudin@hotmail.com>
2020-01-20 15:02:38 +0700

    Add 'data' parameter in the Kemantapan script service.

--- WriteKemantapan/KemantapanToGDB.py
+++ WriteKemantapan/KemantapanToGDB.py
@@ -7,6 +7,7 @@ import os
 os.chdir('E:/SMD_Script')
 
 request = GetParameterAsText(0)
+data = GetParameterAsText(1)
 request_j = json.loads(request)
 with open('smd_config.json') as config_f:
     smd_config = json.load(config_f)
@@ -17,7 +18,7 @@ DataYear = request_j['year']
 
 dbConnection = smd_config['smd_database']['instance']
 
-DataTable = 'SMD.ROUGHNESS_{0}_{1}'.format(DataSemester, DataYear)
+DataTable = 'SMD.{0}_{1}_{2}'.format(data, DataSemester, DataYear)
 RouteID = 'LINKID'
 FromMeasure = 'STA_FROM'
 ToMeasure = 'STA_TO'
@@ -48,5 +49,5 @@ for col_name in summaryTable.dtypes.to_dict():
     columnDetails[col_name]['dtype'] = gdb_dtype  # Insert the column data type
 
 # Write to GDB.
-gdb_table_writer(dbConnection, summaryTable, 'SMD.KEMANTAPAN_ROUGH_{0}_{1}'.format(DataSemester, DataYear),
+gdb_table_writer(dbConnection, summaryTable, 'SMD.KEMANTAPAN_{0}_{1}_{2}'.format(data, DataSemester, DataYear),
                  columnDetails, new_table=False)
