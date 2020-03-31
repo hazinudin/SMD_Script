265079242e625e45b5399adf0a8df8f1220c5a36
hazinudin <hazinudin@hotmail.com>
2020-03-31 14:58:26 +0700

    Add write date feature to gdb_table_writer function.

--- SMD_Package/TableWriter/GDBTableWriter.py
+++ SMD_Package/TableWriter/GDBTableWriter.py
@@ -1,10 +1,11 @@
-from arcpy import da, env, CreateTable_management, AddField_management, Exists
+from arcpy import da, env, CreateTable_management, AddField_management, Exists, ListFields
+import datetime
 
 env.overwriteOutput = True
 
 
 def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=False, input_routeid='LINKID',
-                     target_routeid='LINKID'):
+                     target_routeid='LINKID', write_date=True):
     """
     This function writes input DataFrame as geodatabase event table
     :param workspace: The workspace for target database table
@@ -15,11 +16,13 @@ def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=Fal
     create new column in the newly created table.
     :param input_routeid: The RouteID column in the input table.
     :param target_routeid: The RouteID column in the target table.
+    :param write_date: If True then a date column will be created and filled with current time. Default is True.
     :return:
     """
     env.workspace = workspace  # Environment workspace
     env.overwriteOutput = True
     table_exist = Exists(table_name)  # Check if the table already exist in the workspace.
+    date_column = 'UPDATE_DATE'
 
     if new_table or (not table_exist):  # If the table does not exist or new_table is True, then create new table.
         CreateTable_management(workspace, table_name)
@@ -27,19 +30,30 @@ def gdb_table_writer(workspace, dataframe, table_name, cols_dtype, new_table=Fal
             AddField_management(table_name, col, cols_dtype[col]['dtype'])
 
     input_routes = dataframe[input_routeid].unique().tolist()  # List of every route in the input DataFrame
+    existing_col = [str(f.name) for f in ListFields(table_name)]  # List all available table column
+
+    if write_date and (date_column not in existing_col):  # If the date column does not exist
+        AddField_management(table_name, date_column, "DATE")
 
     for route in input_routes:  # Iterate for every available route in the input
         df_route = dataframe.loc[dataframe[input_routeid] == route]  # The route DataFrame
+
+        if write_date:
+            table_column = cols_dtype.keys()+[date_column]
+        else:
+            table_column = cols_dtype.keys()
 
         with da.UpdateCursor(table_name, target_routeid, where_clause="{0}='{1}'".format(target_routeid, route))\
                 as del_cursor:
             for _ in del_cursor:
                 del_cursor.deleteRow()  # If the route already exist in the table then delete the whole route row
 
-        with da.InsertCursor(table_name, cols_dtype.keys()) as insert_cursor:  # Create an insert cursor
+        with da.InsertCursor(table_name, table_column) as insert_cursor:  # Create an insert cursor
             for index, row in df_route.iterrows():  # Iterate over available rows
-                row_object = []  # Create the new row object
-                for col_name in cols_dtype.keys():
+                row_object = []
+
+                for col_name in cols_dtype.keys():  # Iterate only column which has been specified in config
                     row_object.append(row[col_name])
 
+                row_object.append(datetime.datetime.now())  # Add date value which at the last index
                 insert_cursor.insertRow(row_object)  # Insert the new row
