c7bb3724ae44571cad1625a4eecbd61b73a611aa
hazinudin <hazinudin@hotmail.com>
2020-05-04 15:48:19 +0700

    Change the output table for RoughnessCheck service (Still as a testing table).

--- RoughnessCheck/RoughnessCheck2020.py
+++ RoughnessCheck/RoughnessCheck2020.py
@@ -5,5 +5,5 @@ from SMD_Package.event_table.checks.service import RoughnessCheck
 inputJSON = GetParameterAsText(0)
 forceWrite = GetParameterAsText(1)
 
-RoughnessCheck(forceWrite, input_json=inputJSON, config_path='RoughnessCheck/roughness_config.json',
-               output_table='SMD.IRI_TEST')
+RoughnessCheck(forceWrite, input_json=inputJSON, config_path='RoughnessCheck/roughness_config_2020.json',
+               output_table='SMD.ROUGHNESS_TEST')
