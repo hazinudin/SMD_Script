2e334b1ddcd3aa654fb527401c08b4e570d79aa9
hazinudin <hazinudin@hotmail.com>
2020-05-05 15:05:10 +0700

    Update RNI column details for 2020 period.

--- RNICheck/RNICheck.py
+++ RNICheck/RNICheck.py
@@ -5,5 +5,5 @@ from SMD_Package.event_table.checks.service import RNICheck
 inputJSON = GetParameterAsText(0)
 forceWrite = GetParameterAsText(1)
 
-RNICheck(forceWrite, input_json=inputJSON, config_path='RNICheck/rni_config_2019App.json', output_table='SMD.RNI_TEST',
+RNICheck(forceWrite, input_json=inputJSON, config_path='RNICheck/rni_config_2020.json', output_table='SMD.RNI_TEST',
          semester_data=False)
