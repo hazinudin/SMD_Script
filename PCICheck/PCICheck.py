9a9243e7173e0c25aad7e92024d2232967bfb354
hazinudin <hazinudin@hotmail.com>
2020-05-29 16:05:33 +0700

    Use 2020 configuration file.

--- PCICheck/PCICheck.py
+++ PCICheck/PCICheck.py
@@ -5,4 +5,4 @@ from SMD_Package.event_table.checks.service import PCICheck
 inputJSON = GetParameterAsText(0)
 forceWrite = GetParameterAsText(1)
 
-PCICheck(forceWrite, input_json=inputJSON, config_path='PCICheck/pci_config.json', output_table="SMD.PCI_TEST")
+PCICheck(forceWrite, input_json=inputJSON, config_path='PCICheck/pci_config_2020.json', output_table="SMD.PCI_TEST")
