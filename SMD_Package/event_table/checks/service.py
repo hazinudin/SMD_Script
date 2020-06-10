13aa029bde46e462bbec86c842a99e4183656d04
hazinudin <hazinudin@hotmail.com>
2020-06-10 14:38:55 +0700

    Only execute the measurement check for Deflection if force write parameter is 'false'.

--- SMD_Package/event_table/checks/service.py
+++ SMD_Package/event_table/checks/service.py
@@ -375,7 +375,6 @@ class DeflectionCheck(TableCheckService):
             valid_routes = self.check.valid_route
 
             self.check.range_domain_check(lane_code='SURVEY_DIREC', **self.kwargs)
-            self.check.measurement_check(routes=valid_routes, end_only=True, lane_code=None, **self.kwargs)
             self.check.segment_len_check(routes=valid_routes, lane_code='SURVEY_DIREC', **self.kwargs)
             self.check.median_direction_check(routes=valid_routes, **self.kwargs)
             self.check.surf_thickness_check(**self.kwargs)
@@ -383,6 +382,7 @@ class DeflectionCheck(TableCheckService):
             if str(force_write) == 'false':
                 self.check.coordinate_check(routes=valid_routes, segment_data=True, lat_col='DEFL_LAT', lane_code=None,
                                             long_col='DEFL_LONG', comparison='RNIline-LRS', window=2, **self.kwargs)
+                self.check.measurement_check(routes=valid_routes, end_only=True, lane_code=None, **self.kwargs)
 
             if sorting:
                 deflection = Deflection(self.check.df_valid, 'FORCE', 'FWD', 'FWD_D1', 'FWD_D2', 'ASPHALT_TEMP',
