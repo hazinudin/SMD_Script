81a2330c894bb695f22c0bf9a819173c1b0c3ef2
hazinudin <hazinudin@hotmail.com>
2020-06-30 13:59:22 +0700

    Implement multiprocessing.

--- GetInitialRNI/GetInitialRNI.py
+++ GetInitialRNI/GetInitialRNI.py
@@ -5,27 +5,59 @@ from arcpy import GetParameterAsText
 import json
 import sys
 import os
-sys.path.append('E:\SMD_Script')
-from SMD_Package import input_json_check, event_fc_to_df
+from SMD_Package import input_json_check, event_fc_to_df, SMDConfigs, GetRoutes
+import multiprocessing as mp
+from pandas import concat
 
-os.chdir('E:\SMD_Script')
+if SMDConfigs.smd_dir() == '':
+    pass
+else:
+    os.chdir(SMDConfigs.smd_dir())
 
 # Load the SMD config JSON file
-with open('smd_config.json') as smd_config_f:
-    smd_config = json.load(smd_config_f)
+smd_config = SMDConfigs()
 
 # Get the input JSON from user
 inputJSON = GetParameterAsText(0)
 
 # Load the input JSON
-InputDetails = input_json_check(inputJSON, 1, req_keys=['routes'])
-RouteReq = InputDetails["routes"]
+input_j = input_json_check(inputJSON, 1, req_keys=["prov", "year"])
+prov_req = input_j["prov"]
+data_year = input_j["year"]
+
+# Get the route list
+route_req = GetRoutes('no_prov', str(prov_req)).route_list()
 
 # The SMD config JSON detail
-RNITable = smd_config["table_names"]["rni"]
-RNIRouteID = smd_config["table_fields"]["rni"]["route_id"]
-RNILaneCode = smd_config["table_fields"]["rni"]["lane_code"]
-dbConnection = smd_config["smd_database"]["instance"]
+rni_table = smd_config.table_names["rni"]
+routeid_col = smd_config.table_fields["rni"]["route_id"]
+from_m = smd_config.table_fields["rni"]["from_measure"]
+to_m = smd_config.table_fields["rni"]["to_measure"]
+medwidth = smd_config.table_fields["rni"]["median"]
+road_type = smd_config.table_fields["rni"]["road_type"]
+lane_code = smd_config.table_fields["rni"]["lane_code"]
+db_connection = smd_config.smd_database["instance"]
 
-# Process the request
-RNI_df = event_fc_to_df(RNITable, [RNIRouteID, RNILaneCode], RouteReq, RNIRouteID, dbConnection, is_table=True)
+
+if __name__ == '__main__':
+    # Process the request
+    grp_arg = [[rni_table, [routeid_col, lane_code, from_m, to_m, road_type, medwidth], route, routeid_col,
+                db_connection, True] for route in route_req]
+    result_list = list()
+
+
+    def callback(results):
+        result_list.append(results.to_dict())
+
+    pool = mp.Pool(processes=(mp.cpu_count()-1))
+    for arg in grp_arg:
+        pool.apply_async(event_fc_to_df, args=arg, callback=callback)
+    pool.close()
+    pool.join()
+
+    target_df = concat(result_list)
+    print len(target_df)
+
+    # rni_df = event_fc_to_df(rni_table, [routeid_col, lane_code,
+    #                                     from_m, to_m, road_type, medwidth],
+    #                         route_req, routeid_col, db_connection, is_table=True)
