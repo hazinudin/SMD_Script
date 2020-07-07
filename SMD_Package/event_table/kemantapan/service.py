e8ecbadef0608aebdfc80052d72f8fa3f72c4857
hazinudin <hazinudin@hotmail.com>
2020-07-07 08:06:36 +0700

    Convert time stamp in the route_status dataframe to a string.

--- SMD_Package/event_table/kemantapan/service.py
+++ SMD_Package/event_table/kemantapan/service.py
@@ -162,7 +162,7 @@ class KemantapanService(object):
         self.add_prov_id()
         self.add_balai_id()
 
-        self.status_json = self.route_status.set_index(self.routeid_col).to_json(orient='index')
+        self.status_json = self.route_status.set_index(self.routeid_col).to_json(orient='index', default_handler=str)
         self.write_summary_to_gdb()
 
     def calculate_kemantapan(self, input_df, route):
