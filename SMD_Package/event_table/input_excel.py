3ec32a79b0eb31f5c4f1b1019aa6b9f5d5f43ac1
hazinudin <hazinudin@hotmail.com>
2020-08-10 15:03:35 +0700

    Bug fix, read 'NA' as 'NA' not as pandas NaN. keep_default_na=False.

--- SMD_Package/event_table/input_excel.py
+++ SMD_Package/event_table/input_excel.py
@@ -28,7 +28,7 @@ def read_input_excel(event_table_path, parameter_index=2):
             sys.exit(0)
 
         try:
-            df_string = pd.read_excel(event_table_path, converters=s_converter)  # Convert all column to 'str' type.
+            df_string = pd.read_excel(event_table_path, converters=s_converter, keep_default_na=False)  # Convert all column to 'str' type.
         except UnicodeEncodeError:  # Handle if there is a non ascii character.
             SetParameterAsText(parameter_index, output_message("Failed", "Terdapat karakter yang tidak bisa diconvert."))
             sys.exit(0)
