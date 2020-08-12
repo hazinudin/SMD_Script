4c7be587e9b2e45ced87db3bd10828a0236579db
hazinudin <hazinudin@hotmail.com>
2020-08-12 14:43:09 +0700

    SetParameterAsText for output JSON containing status for each of requested routes.

--- RekapRNI/WriteRekapTipeJalan.py
+++ RekapRNI/WriteRekapTipeJalan.py
@@ -1,8 +1,9 @@
 from SMD_Package.event_table.rni import RNISummary
 from SMD_Package import input_json_check
-from arcpy import GetParameterAsText
+from arcpy import GetParameterAsText, SetParameterAsText
 
 input_text = GetParameterAsText(0)
 input_j = input_json_check(input_text, 1, req_keys=['routes'])
 rekap = RNISummary(**input_j)
 rekap.roadtype_summary(return_df=False)
+SetParameterAsText(1, rekap.status)
