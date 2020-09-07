d760460b04e6081a08a3d55bf29c7a751d01662c
hazinudin <hazinudin@hotmail.com>
2020-09-07 15:45:14 +0700

    Update all RNI summary service script.

--- RekapRNI/WriteRekapLebarJalan.py
+++ RekapRNI/WriteRekapLebarJalan.py
@@ -1,9 +1,8 @@
-from SMD_Package.event_table.rni import RNISummary
+from SMD_Package.event_table.rni.summary import WidthSummary
 from SMD_Package import input_json_check
 from arcpy import GetParameterAsText, SetParameterAsText
 
 input_text = GetParameterAsText(0)
 input_j = input_json_check(input_text, 1, req_keys=['routes'])
-rekap = RNISummary(**input_j)
-rekap.width_summary(return_df=False)
+rekap = WidthSummary(**input_j)
 SetParameterAsText(1, rekap.status)
--- RekapRNI/WriteRekapTipeJalan.py
+++ RekapRNI/WriteRekapTipeJalan.py
@@ -1,9 +1,8 @@
-from SMD_Package.event_table.rni import RNISummary
+from SMD_Package.event_table.rni.summary import RoadTypeSummary
 from SMD_Package import input_json_check
 from arcpy import GetParameterAsText, SetParameterAsText
 
 input_text = GetParameterAsText(0)
 input_j = input_json_check(input_text, 1, req_keys=['routes'])
-rekap = RNISummary(**input_j)
-rekap.roadtype_summary(return_df=False)
+rekap = RoadTypeSummary(**input_j)
 SetParameterAsText(1, rekap.status)
