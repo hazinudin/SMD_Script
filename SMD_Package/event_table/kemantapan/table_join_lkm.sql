80c580951dc5d7b81d752891a5d7c01b0a3653a3
hazinudin <hazinudin@hotmail.com>
2021-01-26 14:26:35 +0700

    RNI and event table join ke for LKM is now multiplied by 100.

--- SMD_Package/event_table/kemantapan/table_join_lkm.sql
+++ SMD_Package/event_table/kemantapan/table_join_lkm.sql
@@ -9,6 +9,6 @@ FROM
         SELECT LINKID, FROM_STA, TO_STA, SURF_TYPE AS SURF_TYPE, LANE_CODE
         FROM rni_2020
     ) rni
-    on table1.LINKID=rni.LINKID and table1.FROM_STA/:to_km_factor = rni.FROM_STA and table1.LANE_CODE = rni.LANE_CODE
+    on table1.LINKID=rni.LINKID and table1.FROM_STA*100/:to_km_factor = rni.FROM_STA and table1.LANE_CODE = rni.LANE_CODE
 WHERE table1.LINKID IN ('01001') -- for testing.
 ORDER by table1.LINKID,table1.FROM_STA,table1.TO_STA
